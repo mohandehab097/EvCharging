886653598be279a07d9e2726d820f4f48fb795ea
mohandehab112 <Honda977@>
2023-07-07 00:22:53 +0200

    relay

--- app/src/main/java/com/example/evchargingapp/ChargeStatusActivity.java
+++ app/src/main/java/com/example/evchargingapp/ChargeStatusActivity.java
@@ -1,14 +1,57 @@
 package com.example.evchargingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.ktx.Firebase;
 
 public class ChargeStatusActivity extends AppCompatActivity {
+    TextView watt,back;
 
+    FirebaseDatabase firebase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charge_status);
+        firebase = FirebaseDatabase.getInstance();
+        watt = findViewById(R.id.watt);
+        back = findViewById(R.id.back);
+
+
+
+        DatabaseReference ref = firebase.getReference("data");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+
+                watt.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChargeStatusActivity.this, HomeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_down_in, R.anim.push_up_out);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/evchargingapp/StartChargeActivity.java
+++ app/src/main/java/com/example/evchargingapp/StartChargeActivity.java
@@ -1,22 +1,70 @@
 package com.example.evchargingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class StartChargeActivity extends AppCompatActivity {
 
     Spinner dropdown;
+    TextView start,stop,back;
+    FirebaseDatabase firebase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_charge);
-         dropdown = findViewById(R.id.spinner1);
-        String[] items = new String[]{"1", "2", "three"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.complaint_dialog, items);
-        dropdown.setAdapter(adapter);
+//         dropdown = findViewById(R.id.spinner1);
+         start = findViewById(R.id.start);
+         stop = findViewById(R.id.stop);
+        back = findViewById(R.id.back);
+
+        firebase = FirebaseDatabase.getInstance();
+        DatabaseReference ref = firebase.getReference("checkRelay");
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.setValue("on");
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.setValue("off");
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartChargeActivity.this, HomeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_down_in, R.anim.push_up_out);
+            }
+        });
+
+
+
+//        String[] items = new String[]{"1", "2", "three"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.complaint_dialog, items);
+//        dropdown.setAdapter(adapter);
+
+
+
+
 
 
     }
